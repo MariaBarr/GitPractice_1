01ec70f5b4d56ac13e55104e6da306004e8493ec
MariaBarr <87657461+MariaBarr@users.noreply.github.com>
2021-07-30 11:05:42 -0400

    Test2

--- src/day4/Test.java
+++ src/day4/Test.java
@@ -2,6 +2,6 @@ package day4;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Tes");
+        System.out.println("Tes"); System.out.println("Tes");
     }
 }
